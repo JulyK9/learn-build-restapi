fabe118b44d4ae9f773ef2223f7c91f48f613111
JKKIM <jkesonjk@gmail.com>
2023-08-31 18:35:56 +0900

    feat: connect mongoose

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,10 @@ import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import { config } from 'dotenv';
+import mongoose from 'mongoose';
+
+config();
 
 const app = express();
 
@@ -22,4 +26,15 @@ const server = http.createServer(app);
 
 server.listen(8080, () => {
   console.log(`Server running on http://localhost:8080/`);
+});
+
+const MONGO_URL = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PW}@boilerplate.axivy.mongodb.net/`;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+// mongoose 연결 확인용
+mongoose.connection.once('open', () => {
+  console.log('connected to MongoDB');
 });
