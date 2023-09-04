49d467c4c076f163ae50666bd7885e544324bca4
JKKIM <jkesonjk@gmail.com>
2023-09-04 20:48:57 +0900

    feat: add connect login controller to router

--- src/router/authentication.ts
+++ src/router/authentication.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { register } from '../controllers/authentication';
+import { login, register } from '../controllers/authentication';
 
 // 해당 엔드포인트에 post 요청이 들어오면 어떤 컨트롤러 함수를 싫행할지 정의하는 부분
 // /auth/register 엔드포인트로 post 요청이 들어오면 register 컨트롤러 함수를 실행함
 // register 함수는 사용자 등록을 처리하고 응답을 반환하게 됨
 export default (router: express.Router) => {
   router.post('/auth/register', register);
+  router.post('/auth/login', login);
 };
