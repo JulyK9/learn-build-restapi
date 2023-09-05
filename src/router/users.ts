b4c8ac9697a30b680ad651b65001928429ec353b
JKKIM <jkesonjk@gmail.com>
2023-09-05 16:55:19 +0900

    feat: add isAuthenticated middleware to users router

--- src/router/users.ts
+++ src/router/users.ts
@@ -1,7 +1,9 @@
 import express from 'express';
 
 import { getAllUsers } from '../controllers/users';
+import { isAuthenticated } from '../middlewares';
 
+// 인증된 유저만 접근할 수 있도록 isAuthenticated 미들웨어 추가
 export default (router: express.Router) => {
-  router.get('/users', getAllUsers);
+  router.get('/users', isAuthenticated, getAllUsers);
 };
